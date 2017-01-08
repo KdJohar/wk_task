04212ce938ba9d5dbff0c134ff2735b31a45d507
Kd Johar <kdjohar91@gmail.com>
2017-01-08 16:57:12 +0530

    models added for project

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Categories, SubCategories, Product
 # Register your models here.
+admin.site.register(Categories)
+admin.site.register(SubCategories)
+admin.site.register(Product)
--- web/models.py
+++ web/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class SubCategories(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(Categories)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    subcategory = models.ForeignKey(SubCategories)
+
+    def __unicode__(self):
+        return self.name
